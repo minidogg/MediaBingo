ecdef832068473c1fe5aaa1e42e1530a9d697488
minidogg <117037081+minidogg@users.noreply.github.com>
2025-07-21 15:29:38 +0000

    upload data file

--- src/src/main.ts
+++ src/src/main.ts
@@ -5,13 +5,25 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = /*html*/`
     <div id="menu">
       <h1>Media Bingo</h1>
-      <input type="file" name="" id="uploaddata">
+      <input type="file" name="" id="uploaddata" accept="application/json">
       <button id="gen" disabled>Generate Cards</button>
       <button id="bingo" disabled>Bingo</button>
     </div>
 `
-
+const menu = document.querySelector<HTMLDivElement>("#menu")
 const uploadInput = app.querySelector<HTMLInputElement>("#uploaddata")
-uploadInput?.addEventListener("change", ()=>{
-  console.log("upload")
+uploadInput?.addEventListener("change", async(ev)=>{
+  if(!uploadInput.files)return;
+  let json = {}
+  try{
+    json = JSON.parse(await uploadInput.files[0].text())
+  }catch(err){
+    alert(err)
+    return;
+  }
+  console.log("upload", json)
+
+  for(let item of menu?.querySelectorAll("button")!){
+    item.disabled = false
+  }
 })
